20f4b875405df64791d55c2dd621eb595a29d532
pt  qu <ptqu@betawm.com>
2022-10-20 13:30:44 +0800

    1

--- helloworld/helloworld/src/main.rs
+++ helloworld/helloworld/src/main.rs
@@ -1,4 +1,4 @@
-fn greet_world(a : int32) -> Result<string,bool> {
+fn greet_world(a: u32) -> Result<String, bool> {
     let southern_germany = "Grüß Gott!";
     let chinese = "世界，你好";
     let english = "World, hello";
@@ -6,10 +6,11 @@ fn greet_world(a : int32) -> Result<string,bool> {
     for region in regions.iter() {
         println!("{}", &region);
     }
-if(a==1)
-   Ok("123")
-   else
-    Err(false)
+    if a == 1 {
+        Ok("1230".to_string())
+    } else {
+        Err(false)
+    }
 }
 
 fn main() {
@@ -18,5 +19,10 @@ fn main() {
         v = if i == 9 { continue } else { i }
     }
     println!("{}", v);
-    greet_world();
+    let gg = greet_world(3);
+
+    match gg {
+        Ok(t) => println!("{}", t),
+        Err(e) => println!("{}", e),
+    }
 }
